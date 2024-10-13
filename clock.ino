9d163962ebdd96d3f99b4d37a66eb703cb4298a8
Philipp Bruhns <philippbruhns@eduroam-int-dhcp-97-19-176.ucl.ac.uk>
2024-10-13 22:35:02 +0100

    working version

--- clock.ino
+++ clock.ino
@@ -1,16 +1,66 @@
+// Credits:
+// All interaction with the TFT Display is based on Example TFT_eSPI/Test and Diagnostics/Colour_Test
+// All time retrieving functions are based on Example ESP32/Time/SimpleTime
+// The Wifi functionality is implemented according to the Connecting to eduroam example provided on Moodle
+
 #include <WiFi.h>
 #include "esp_eap_client.h"
-#include <HTTPClient.h>
-#include <Arduino_JSON.h>
 
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+#include "time.h"
+#include "esp_sntp.h"
+
+
+TFT_eSPI tft = TFT_eSPI();
+
+// WIFI authentication vars
 const char* USER = "zcabplb@ucl.ac.uk";
 const char* PASS = "";
 
-const char* url = "http://worldtimeapi.org/api/timezone/Europe/London";
+// time ntp servers and time zone localisation
+const char *ntpServer1 = "pool.ntp.org";
+const char *ntpServer2 = "time.nist.gov";
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 3600;
+
+// prints current local time to both tft display and console 
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("No time available (yet)");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B, %d, %Y, %H:%M:%S");
+
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 4, 4);
+  tft.println(&timeinfo, "%A");
+  tft.println(&timeinfo, "%B, %d");
+  tft.println(&timeinfo, "%Y");
+  tft.println(&timeinfo, "%H:%M:%S");
+}
+
+// see Example ESP32/Time/SimpleTime
+// Callback function (gets called when time adjusts via NTP)
+void timeavailable(struct timeval *t) {
+  Serial.println("Got time adjustment from NTP!");
+  //printLocalTime();
+}
 
 void setup() {
+  tft.init();
   Serial.begin(115200);
-  // put your setup code here, to run once:
+
+
+  // Display Setup
+  tft.init();
+  tft.fillScreen(TFT_BLACK);
+  tft.setCursor(0, 4, 4);
+  tft.setTextColor(TFT_WHITE);
+
+  // WIFI Setup
   WiFi.mode(WIFI_STA);
   esp_eap_client_set_identity((uint8_t *)USER, strlen(USER));
   esp_eap_client_set_username((uint8_t *)USER, strlen(USER));
@@ -19,7 +69,6 @@ void setup() {
   WiFi.begin("eduroam");
 
   Serial.print("Connecting");
-  String res;
 
   while ((WiFi.status() != WL_CONNECTED)) {
     delay(500);
@@ -31,48 +80,15 @@ void setup() {
   Serial.print("Connected to WiFi network with IP Adress: ");
   Serial.println(WiFi.localIP());
 
-  if (WiFi.status()== WL_CONNECTED) {
-
-    res = httpGETRequest(url);
-    Serial.println(res);
-
-    JSONVar time_info = JSON.parse(res);
-
-  }
+  // Time Setup
+  sntp_set_time_sync_notification_cb(timeavailable);
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer1, ntpServer2);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-}
+  delay(1000);
+  printLocalTime();
 
-String httpGETRequest(const char* serverName) {
-  WiFiClient client;
-  HTTPClient http;
-    
-  // Your Domain name with URL path or IP address with path
-  http.begin(client, serverName);
-  
-  // If you need Node-RED/server authentication, insert user and password below
-  //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-  
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-  
-  String payload = "{}"; 
-  
-  if (httpResponseCode>0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
-  http.end();
-
-  return payload;
 }
